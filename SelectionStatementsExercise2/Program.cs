00edda78336f8cf55239f096d716e67276d9d79f
Peter Kalinowski <petekal1323@gmail.com>
2025-03-24 17:44:08 -0400

    favSubject exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+            
+            Console.WriteLine("Please enter to continue...");
+            Console.ReadKey();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("It's okay!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is great!");
+                    break;
+                case "english":
+                    Console.WriteLine("Eh, it's okay.");
+                    break;
+                case "history":
+                    Console.WriteLine("I like History");
+                    break;
+                default:
+                    Console.WriteLine("That's a great subject!");
+                    break;
+                
+            }
         }
     }
 }
